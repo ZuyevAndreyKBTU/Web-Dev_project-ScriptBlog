1e46cb4cb970c770ab282ea5bdea9ca24c638674
ZuyevAndreyKBTU <zuev.andrei@phystech.edu>
2024-04-25 04:23:55 +0500

    installed simpleJWt and setted up

--- backend/blog/blog/settings.py
+++ backend/blog/blog/settings.py
@@ -27,6 +27,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 
 # Application definition
 
