f0b46e49ffdbfd536634827fdc739ce51d102542
ZuyevAndreyKBTU <zuev.andrei@phystech.edu>
2024-04-25 03:47:47 +0500

    created Category model

--- backend/blog/api/models.py
+++ backend/blog/api/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank=False, default='')
+    owner = models.ForeignKey('auth.User', related_name='categories', on_delete=models.CASCADE)
+    posts = models.ManyToManyField('Post', related_name='categories', blank=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+        
 class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=76, blank=True, default='Script for ...')
