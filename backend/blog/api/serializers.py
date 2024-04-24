937021b82d3696796fc24db030240aa2aa80dc02
ZuyevAndreyKBTU <zuev.andrei@phystech.edu>
2024-04-25 03:55:36 +0500

    added category serializer

--- backend/blog/api/serializers.py
+++ backend/blog/api/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from api.models import Post
 from api.models import Comment
+from api.models import Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'owner', 'posts']
 
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -16,12 +25,15 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'body', 'owner', 'comments']
+        fields = ['id', 'title', 'body', 'owner', 'comments', 'categories']
 
 class UserSerializer(serializers.ModelSerializer):
     posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
+    categories = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+#i didn't fully understanded why do we need to add categories into user serializer
+#but outputting all posts in defined category from this user. Also, all people in the internet 
+#do it, then i will too
     class Meta:
         model = User
-        fields = ['id', 'username', 'posts', 'comments']
+        fields = ['id', 'username', 'posts', 'comments', 'categories']
