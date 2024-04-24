be78bc8edd7193a2c75abf8c2dd14d87b1ffe854
ZuyevAndreyKBTU <zuev.andrei@phystech.edu>
2024-04-25 04:20:57 +0500

    added endpoint for Claps

--- backend/blog/api/urls.py
+++ backend/blog/api/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('comments/<int:pk>/', views.CommentDetail.as_view()),
     path('categories/', views.CategoryList.as_view()),
     path('categories/<int:pk>/', views.CategoryDetail.as_view()),
+    path('posts/<int:post_id>/clap/', views.ClapCreateView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
