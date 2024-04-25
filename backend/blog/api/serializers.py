18647381a2f6728e08682dfe1aab338e54172905
ZuyevAndreyKBTU <zuev.andrei@phystech.edu>
2024-04-25 05:07:00 +0500

    rewrited serializers to follow requirements

--- backend/blog/api/serializers.py
+++ backend/blog/api/serializers.py
@@ -5,42 +5,71 @@ from api.models import Comment
 from api.models import Category
 from api.models import Clap
 
-class ClapSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Clap
-        fields = ['id', 'post', 'count']
-
-class CategorySerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
-    class Meta:
-        model = Category
-        fields = ['id', 'name', 'owner', 'posts']
-
-class CommentSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=True, allow_blank=False, max_length=100)
+    body = serializers.CharField(style={'base_template': 'textarea.html'})
     owner = serializers.ReadOnlyField(source='owner.username')
 
-    class Meta:
-        model = Comment
-        fields = ['id', 'body', 'owner', 'post']
+    def create(self, validated_data):
+        return Post.objects.create(**validated_data)
 
-class PostSerializer(serializers.ModelSerializer):
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.body = validated_data.get('body', instance.body)
+        instance.save()
+        return instance
+
+class CommentSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    body = serializers.CharField(required=True, allow_blank=False, max_length=300)
     owner = serializers.ReadOnlyField(source='owner.username')
-    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    claps = ClapSerializer(many=True, read_only=True)
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
 
-    class Meta:
-        model = Post
-        fields = ['id', 'title', 'body', 'owner', 'comments', 'categories', 'claps']
+    def create(self, validated_data):
+        return Comment.objects.create(**validated_data)
 
-class UserSerializer(serializers.ModelSerializer):
-    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    categories = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-#i didn't fully understanded why do we need to add categories into user serializer
-#but outputting all posts in defined category from this user. Also, all people in the internet 
-#do it, then i will too
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'posts', 'comments', 'categories']
+    def update(self, instance, validated_data):
+        instance.body = validated_data.get('body', instance.body)
+        instance.save()
+        return instance
+    
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(max_length=150)
+    email = serializers.EmailField()
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+        return instance
+
+class CategorySerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(required=True, max_length=100)
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+    
+class ClapSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    count = serializers.IntegerField(default=0)
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+
+    def create(self, validated_data):
+        return Clap.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.count = validated_data.get('count', instance.count)
+        instance.save()
+        return instance
